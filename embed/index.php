d836b2d1c551b2f5306f33595f3612175b79fcb8
RoadRunner-Dollars <RoadRunner.Dollars@gmail.com>
2016-06-30 01:52:21 -0700

    Add files via upload

---
+++ embed/index.php
@@ -0,0 +1,4 @@
+<center><textarea rows="4" cols="85.99">
+<body bgcolor="black"><center><iframe src="http://roadrunner-forums.com/chat/index.php" style="border:0px #FFFFFF none;" name="chat" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="900px" width="620px"></iframe></center></body> 
+</textarea></center><p>
+<body bgcolor="black"><center><iframe src="http://roadrunner-forums.com/chat/index.php" style="border:0px #FFFFFF none;" name="chat" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="900px" width="620px"></iframe></center></body>
